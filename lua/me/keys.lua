a4b6979b7d92e67053dce24088b69e9868bc05c1
axvg <91773368+axvg@users.noreply.github.com>
2023-06-24 02:58:55 -0500

    feat: update netrw keys

--- lua/me/keys.lua
+++ lua/me/keys.lua
@@ -1,3 +1,2 @@
 vim.g.mapleader = " "
--- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>d", function() if (vim.api.nvim_buf_get_option(0, "filetype")=="netrw") then vim.api.nvim_exec("close", false) else vim.api.nvim_exec(":Vexplore", false) end end, {})
+vim.keymap.set("n", "<leader>pv", function() if (vim.api.nvim_buf_get_option(0, "filetype")=="netrw") then vim.api.nvim_exec("close", false) else vim.api.nvim_exec(":Vexplore", false) end end, {})
